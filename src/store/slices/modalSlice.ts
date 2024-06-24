d85cd1610cf421704ce794a2727f2a819759dd60
ChanGyunLee <lgyn10@naver.com>
2024-06-25 08:54:40 +0900

    feat: modalSlice.ts 코드 구현

---
+++ src/store/slices/modalSlice.ts
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { ITask } from '../../types';
+
+type TModalState = {
+  boardId: string;
+  listId: string;
+  task: ITask;
+};
+
+const initialState: TModalState = {
+  boardId: 'board-0',
+  listId: 'list-0',
+  task: {
+    taskId: 'task-0',
+    taskName: 'task 0',
+    taskDescription: 'task description',
+    taskOwner: 'lgyn10',
+  },
+};
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState,
+  // reducers 안에는 action을 만들어주는 함수를 적어준다.
+  reducers: {},
+});
+
+export const modalReducer = modalSlice.reducer;
---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+// modalSlice.ts에서 쓰기 위함
+export interface ITask {
+  taskId: string;
+  taskName: string;
+  taskDescription: string;
+  taskOwner: string;
+}
