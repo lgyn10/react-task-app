6db3c36068bb19aaa052968d66f992409aef271b
ChanGyunLee <lgyn10@naver.com>
2024-06-24 14:02:54 +0900

    chore: 프로젝트 요구사항에 맞는 폴더 구조 변경

---
+++ src/components/LoggerModal/Logitem/Logitem.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Logitem = () => {
+  return <div>Logitem</div>;
+};
+
+export default Logitem;
---
+++ src/hooks/redux.ts
@@ -0,0 +1,1 @@
+// useSelector, useDispatch 는 ts에서 hooks로 만들어 사용하는 것이 편하다.
