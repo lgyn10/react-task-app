6c0269c1bb7c3b6b337422e340732aab36650711
ChanGyunLee <lgyn10@naver.com>
2024-06-25 10:06:03 +0900

    feat: Board 데이터 및 타입 생성

--- src/store/slices/boardsSlice.ts
+++ src/store/slices/boardsSlice.ts
@@ -1,8 +1,57 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { IBoard } from '../../types';
 
-const initialState = {
+type TBoardState = {
+  modalActive: boolean;
+  boardArray: IBoard[];
+};
+
+const initialState: TBoardState = {
   modalActive: false,
-  boardArray: [],
+  boardArray: [
+    {
+      boardId: 'board-0',
+      boardName: '첫 번째 게시물',
+      lists: [
+        {
+          listId: 'list-0',
+          listName: 'List 1',
+          tasks: [
+            {
+              taskId: 'task-0',
+              taskName: 'Task 1',
+              taskDescription: 'Description',
+              taskOwner: 'lgyn10',
+            },
+            {
+              taskId: 'task-1',
+              taskName: 'Task 2',
+              taskDescription: 'Description',
+              taskOwner: 'lgyn10',
+            },
+          ],
+        },
+        {
+          listId: 'list-1',
+          listName: 'List 2',
+          tasks: [
+            {
+              taskId: 'task-2',
+              taskName: 'Task 3',
+              taskDescription: 'Description',
+              taskOwner: 'lgyn10',
+            },
+            {
+              taskId: 'task-3',
+              taskName: 'Task 4',
+              taskDescription: 'Description',
+              taskOwner: 'lgyn10',
+            },
+          ],
+        },
+      ],
+    },
+  ],
 };
 
 const boardSlice = createSlice({
