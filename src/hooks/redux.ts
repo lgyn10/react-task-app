ed472d4d817daf57f792042427e430b21d69ac6c
ChanGyunLee <lgyn10@naver.com>
2024-06-25 11:07:45 +0900

    feat: 리덕스 Hooks 생성

--- src/hooks/redux.ts
+++ src/hooks/redux.ts
@@ -1,1 +1,35 @@
 // useSelector, useDispatch 는 ts에서 hooks로 만들어 사용하는 것이 편하다.
+
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../store';
+
+// 아래 두 함수는 커스텀 훅..?
+export const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+export const useTypedDispatch = () => useDispatch<AppDispatch>();
+
+// const dispatch = useDispatch();
+// const logger = useTypedSelector((state) => state.logger);
+// 타입스크립트가 추론하지 못할 경우, 개발자가 직접 타입을 지정해야 한다. (annotate)
+
+// store 객체를 이용해서 타입을 가져올 수 있다.
+
+// 제네릭 관련
+// interface Obj<T> {
+//   name: T;
+// }
+
+// interface State {
+//   state: {
+//     data: string;
+//     loding: boolean;
+//   };
+// }
+
+// const obj: Obj<State> = {
+//   name: {
+//     state: {
+//       data: 'data',
+//       loding: true,
+//     },
+//   },
+// };
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,4 +5,11 @@ const store = configureStore({
   reducer: reducer, // reducer,
 });
 
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+// useAppSelector 또는 useTypedSelector으로 명시
+
+// store.getState();
+// 위 코드를 사용하면 store의 데이터를 가져올 수 있다.
+
 export default store;
