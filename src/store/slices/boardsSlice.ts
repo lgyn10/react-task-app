8e01b2a4d4d4ab62d52e3388c759e882f516dbc2
ChanGyunLee <lgyn10@naver.com>
2024-06-25 09:08:57 +0900

    feat: boardsSlice.ts 코드 구현

---
+++ src/store/slices/boardsSlice.ts
@@ -0,0 +1,14 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  modalActive: false,
+  boardArray: [],
+};
+
+const boardSlice = createSlice({
+  name: 'boards',
+  initialState,
+  reducers: {},
+});
+
+export const boardsReducer = boardSlice.reducer;
