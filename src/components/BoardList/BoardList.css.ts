e9db7935648108b4d266a8d107b905c10d5ea6a9
ChanGyunLee <lgyn10@naver.com>
2024-06-26 13:23:38 +0900

    style: BoradList 스타일 구현

---
+++ src/components/BoardList/BoardList.css.ts
@@ -0,0 +1,68 @@
+import { style } from '@vanilla-extract/css';
+import { vars } from '../../App.css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'row',
+  alignItems: 'center',
+  flexWrap: 'wrap', // warp하면 넓이가 좁혀질 때 컴포넌트가 아래로 내려간다.
+  rowGap: 15,
+  minHeight: 'max-content',
+  padding: vars.spacing.big2,
+  backgroundColor: vars.color.mainDarker,
+});
+
+export const title = style({
+  color: vars.color.brightText,
+  fontSize: vars.fontSizing.T2,
+  marginRight: vars.spacing.big1,
+});
+
+export const addButton = style({
+  color: vars.color.brightText,
+  fontSize: vars.fontSizing.T2,
+  cursor: 'pointer',
+  marginLeft: vars.spacing.big1,
+  transition: '0.3s',
+  ':hover': {
+    color: 'red',
+    opacity: 0.8,
+    transition: '0.3s',
+    transform: 'scale(1.2)',
+  },
+});
+
+export const boardItem = style({
+  color: vars.color.brightText,
+  fontSize: vars.fontSizing.T3,
+  backgroundColor: vars.color.mainFaded,
+  padding: vars.spacing.medium,
+  borderRadius: 10,
+  cursor: 'pointer',
+  marginRight: vars.spacing.big1,
+  ':hover': {
+    opacity: 0.8,
+    transform: 'scale(1.03)',
+  },
+});
+
+export const boardItemActive = style({
+  color: vars.color.brightText,
+  fontSize: vars.fontSizing.T3,
+  backgroundColor: vars.color.selectedTab,
+  padding: vars.spacing.medium,
+  borderRadius: 10,
+  cursor: 'pointer',
+  marginRight: vars.spacing.big1,
+});
+
+export const addSection = style({
+  display: 'flex',
+  alignItems: 'center',
+  marginLeft: 'auto',
+});
+
+export const smallTitle = style({
+  color: vars.color.brightText,
+  fontSize: vars.fontSizing.T3,
+});
