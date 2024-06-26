2894a202c93d722c2a991113486390dbad3a6707
ChanGyunLee <lgyn10@naver.com>
2024-06-26 14:20:40 +0900

    feat: SideForm 렌더링 시 input에 포커싱
    useRef와 setTimeout 함수를 활용한 기법

--- src/components/BoardList/BoardList.tsx
+++ src/components/BoardList/BoardList.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from 'react';
+import React, { FC, useRef, useState } from 'react';
 import { useTypedSelector } from '../../hooks/redux';
 import SideForm from './SideForm/SideForm';
 import { FiPlusCircle } from 'react-icons/fi';
@@ -14,8 +14,15 @@ const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => 
   // 구조분해할당
   const { boardArray } = useTypedSelector((state) => state.boards);
   // 대안 : const boards = useTypedSelector((state) => state.boards.boardArray);
-
   const [isFormOpen, setIsFormOpen] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    setIsFormOpen(!isFormOpen);
+    setTimeout(() => {
+      inputRef.current?.focus();
+    }, 0);
+  };
 
   return (
     <div className={container}>
@@ -40,9 +47,9 @@ const BoardList: FC<TBoardListProps> = ({ activeBoardId, setActiveBoardId }) => 
       })}
       <div className={addSection}>
         {isFormOpen ? (
-          <SideForm setIsFormOpen={setIsFormOpen} />
+          <SideForm inputRef={inputRef} setIsFormOpen={setIsFormOpen} />
         ) : (
-          <FiPlusCircle className={addButton} onClick={() => setIsFormOpen(!isFormOpen)} />
+          <FiPlusCircle className={addButton} onClick={handleClick} />
         )}
       </div>
     </div>
--- src/components/BoardList/SideForm/SideForm.tsx
+++ src/components/BoardList/SideForm/SideForm.tsx
@@ -3,21 +3,23 @@ import { FiCheckCircle } from 'react-icons/fi';
 
 type TSideFormProps = {
   setIsFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  inputRef: React.RefObject<HTMLInputElement>;
 };
 
-const SideForm: FC<TSideFormProps> = ({ setIsFormOpen }) => {
+const SideForm: FC<TSideFormProps> = ({ setIsFormOpen, inputRef }) => {
   const [inputText, setInputText] = useState('');
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputText(e.target.value);
   };
-
   const handleOnBlur = () => {
     setIsFormOpen(false);
   };
+
   return (
     <div>
       <input
+        ref={inputRef}
         type='text'
         placeholder='새로운 게시판 등록하기'
         value={inputText}
