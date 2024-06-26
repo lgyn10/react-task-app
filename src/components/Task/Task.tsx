a4b17080e4b0d5d8505e76a566e8e7908c4f6b43
ChanGyunLee <lgyn10@naver.com>
2024-06-26 22:21:38 +0900

    feat: Task 컴포넌트 코드 구현

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { container, description, title } from './Task.css';
 
-const Task = () => {
-  return <div>Task</div>;
+type TTaskProps = {
+  taskName: string;
+  taskDescription: string;
+  boardId: string;
+  index: number;
+};
+
+const Task: FC<TTaskProps> = ({ taskName, taskDescription, boardId, index }) => {
+  return (
+    <div className={container}>
+      <div className={title}>{taskName}</div>
+      <div className={description}>{taskDescription}</div>
+    </div>
+  );
 };
 
 export default Task;
