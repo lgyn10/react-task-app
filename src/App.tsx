944ccf62d7fdea14f48e6550790bb169e3aaa3c6
ChanGyunLee <lgyn10@naver.com>
2024-06-27 13:32:51 +0900

    feat: Modal Edit 컴포넌트 코드 구현

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { appContainer, board, buttons } from './App.css';
 import BoardList from './components/BoardList/BoardList';
 import ListsContainer from './components/ListsContainer/ListsContainer';
 import { useTypedSelector } from './hooks/redux';
+import EditModal from './components/EditModal/EditModal';
 
 function App() {
   const [activeBoardId, setActiveBoardId] = useState('board-1');
+
+  const modalActive = useTypedSelector((state) => state.boards.modalActive);
 
   const boards = useTypedSelector((state) => state.boards.boardArray);
   const getActiveBoard = boards.filter((board) => board.boardId === activeBoardId)[0];
@@ -13,6 +16,8 @@ function App() {
 
   return (
     <div className={appContainer}>
+      {/*//! 모달창 구현 */}
+      {modalActive ? <EditModal /> : null}
       <BoardList activeBoardId={activeBoardId} setActiveBoardId={setActiveBoardId} />
       <div className={board}>
         <ListsContainer lists={lists} boardId={getActiveBoard.boardId} />
--- src/components/EditModal/EditModal.tsx
+++ src/components/EditModal/EditModal.tsx
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FiXCircle } from 'react-icons/fi';
+import { useTypedDispatch, useTypedSelector } from '../../hooks/redux';
+import { setModalActive } from '../../store/slices/boardsSlice';
 
 const EditModal = () => {
-  return <div>EditModal</div>;
+  const editingState = useTypedSelector((state) => state.modal);
+  const [modalData, setModalData] = useState(editingState);
+
+  const dispatch = useTypedDispatch();
+
+  const handleCloseButton = () => {
+    dispatch(setModalActive(false));
+  };
+
+  return (
+    <>
+      <div>
+        <div>
+          {/* {modalData.task.taskName}와 차이는? */}
+          <div>{editingState.task.taskName}</div>
+
+          <FiXCircle onClick={handleCloseButton} />
+          <div>제목</div>
+          <input type='text' value={modalData.task.taskName} />
+          <div>설명</div>
+          <input type='text' value={modalData.task.taskDescription} />
+          <div>생성한 사람</div>
+          <input type='text' value={modalData.task.taskOwner} />
+          <div>
+            <button>할 일 수정하기</button>
+            <button>할 일 삭제하기</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default EditModal;
