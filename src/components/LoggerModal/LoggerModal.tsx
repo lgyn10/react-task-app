d9f716eed30881462f90db2719db0a55d7762a49
ChanGyunLee <lgyn10@naver.com>
2024-06-27 18:00:17 +0900

    feat: LoggerModal 컴포넌트 코드 구현

--- src/components/LoggerModal/LoggerModal.tsx
+++ src/components/LoggerModal/LoggerModal.tsx
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { useTypedSelector } from '../../hooks/redux';
+import { FiXCircle } from 'react-icons/fi';
+import Logitem from './Logitem/Logitem';
+import { body, closeButton, header, modalWindow, title, wrapper } from './LoggerModal.css';
 
-const LoggerModal = () => {
-  return <div>LoggerModal</div>;
+type TLoggerModalProps = {
+  setIsLoggerOpen: React.Dispatch<React.SetStateAction<boolean>>;
+};
+
+const LoggerModal: FC<TLoggerModalProps> = ({ setIsLoggerOpen }) => {
+  const logs = useTypedSelector((state) => state.logger.logArray);
+
+  return (
+    <div className={wrapper}>
+      <div className={modalWindow}>
+        <div className={header}>
+          <div className={title}>활동 기록</div>
+          <FiXCircle className={closeButton} onClick={() => setIsLoggerOpen(false)} />
+        </div>
+        <div className={body}>
+          {logs.map((log) => {
+            return <Logitem key={log.logId} logItem={log} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoggerModal;
