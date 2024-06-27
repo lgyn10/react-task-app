7a3e89638ddd7401ce10ad2ff3326c0be9f2380d
ChanGyunLee <lgyn10@naver.com>
2024-06-28 08:40:53 +0900

    fix: Logitem -> LogItem

--- src/components/LoggerModal/LoggerModal.tsx
+++ src/components/LoggerModal/LoggerModal.tsx
@@ -1,7 +1,7 @@
 import React, { FC } from 'react';
 import { useTypedSelector } from '../../hooks/redux';
 import { FiXCircle } from 'react-icons/fi';
-import Logitem from './Logitem/Logitem';
+import Logitem from './LogItem/LogItem';
 import { body, closeButton, header, modalWindow, title, wrapper } from './LoggerModal.css';
 
 type TLoggerModalProps = {
