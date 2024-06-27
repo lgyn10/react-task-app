b1e3756ef7c8b3f1779602b5c6bf754f387790bc
ChanGyunLee <lgyn10@naver.com>
2024-06-28 08:21:57 +0900

    feat: LogItem 컴포넌트 코드 구현

--- src/components/LoggerModal/Logitem/Logitem.tsx
+++ src/components/LoggerModal/Logitem/Logitem.tsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { ILogItem } from '../../../types';
+import { BsFillPersonFill } from 'react-icons/bs';
 
-const Logitem = () => {
-  return <div>Logitem</div>;
+type TLogItemProps = {
+  logItem: ILogItem;
+};
+
+const Logitem: FC<TLogItemProps> = ({ logItem }) => {
+  const timeOffset = new Date(Date.now() - Number(logItem.logTimeStamp));
+  console.log(timeOffset);
+  console.log(timeOffset.getMinutes());
+  console.log(timeOffset.getSeconds());
+
+  const showOffsetTime = `
+  ${timeOffset.getMinutes() > 0 ? `${timeOffset.getMinutes()}m` : ``}
+  ${timeOffset.getSeconds() > 0 ? `${timeOffset.getSeconds()}s ago` : ``}
+  ${timeOffset.getSeconds() === 0 ? `just now` : ''}
+  `;
+
+  return (
+    <div>
+      <div>
+        <BsFillPersonFill />
+        {logItem.logAuthor}
+      </div>
+      <div>{logItem.logMessage}</div>
+      <div>{showOffsetTime}</div>
+    </div>
+  );
 };
 
 export default Logitem;
