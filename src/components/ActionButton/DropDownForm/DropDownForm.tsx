6933047d58107ece62e96f6ffd7f9661f162415b
ChanGyunLee <lgyn10@naver.com>
2024-06-27 10:18:15 +0900

    feat: DropDownForm 컴포넌트 코드 구현

--- src/components/ActionButton/DropDownForm/DropDownForm.tsx
+++ src/components/ActionButton/DropDownForm/DropDownForm.tsx
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { ChangeEvent, Dispatch, FC, SetStateAction, useState } from 'react';
+import { FiXCircle } from 'react-icons/fi';
+import { useTypedDispatch } from '../../../hooks/redux';
+import { addList, addTask } from '../../../store/slices/boardsSlice';
+import { v4 } from 'uuid';
+import { addLog } from '../../../store/slices/loggerSlice';
 
-const DropDownForm = () => {
-  return <div>DropDownForm</div>;
+type TDropButtonProps = {
+  setIsFormOpen: Dispatch<SetStateAction<boolean>>;
+  // setIsFormOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  boardId: string;
+  listId: string;
+  list?: boolean;
+};
+
+const DropDownForm: FC<TDropButtonProps> = ({ setIsFormOpen, list, boardId, listId }) => {
+  const [text, setText] = useState('');
+  const dispatch = useTypedDispatch();
+
+  const FormPlaceHolder = list ? '리스트의 제목을 입력하세요.' : '할 일의 제목을 입력하세요.';
+  const buttonTitle = list ? '리스트 추가하기' : '할 일 추가하기';
+
+  const handleTextChagne = (e: ChangeEvent<HTMLTextAreaElement>) => {
+    setText(e.target.value);
+  };
+  const handleButtonClick = () => {
+    if (text) {
+      if (list) {
+        dispatch(addList({ boardId, list: { listId: v4(), listName: text, tasks: [] } }));
+        dispatch(
+          addLog({
+            logId: v4(),
+            logMessage: `리스트 생성하기 : ${text}`,
+            logAuthor: 'lgyn10',
+            logTimeStamp: String(Date.now()),
+          })
+        );
+      } else {
+        dispatch(
+          addTask({
+            boardId,
+            listId,
+            task: {
+              taskId: v4(),
+              taskName: text,
+              taskDescription: '',
+              taskOwner: 'lgyn10',
+            },
+          })
+        );
+        dispatch(
+          addLog({
+            logId: v4(),
+            logMessage: `할 일 생성하기 : ${text}`,
+            logAuthor: 'lgyn10',
+            logTimeStamp: String(Date.now()),
+          })
+        );
+      }
+    }
+  };
+
+  return (
+    <div>
+      <textarea
+        value={text}
+        autoFocus
+        placeholder={FormPlaceHolder}
+        onChange={handleTextChagne}
+        onBlur={() => setIsFormOpen(false)}
+      />
+      <div>
+        <button onMouseDown={handleButtonClick}>{buttonTitle}</button>
+        <FiXCircle />
+      </div>
+    </div>
+  );
 };
 
 export default DropDownForm;
